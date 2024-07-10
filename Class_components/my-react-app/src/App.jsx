049f1a7d273e58daa9cc08ef1b8a684fc05d15f7
Daniil Zaburunov <zaburunov.2000@mail.ru>
2024-07-10 21:51:11 +0400

    Жизненный цилк компонента, запрос по адресу и получение данных с промежутком времени

--- Class_components/my-react-app/src/App.jsx
+++ Class_components/my-react-app/src/App.jsx
@@ -1,87 +1,44 @@
 import React from "react";
 import "./App.css";
-//Мой вариант начало
-// class App extends React.Component {
-//   state = {
-//     quantity: 1,
-//     show: true,
-//     max: 5,
-//     min: 0,
-//   };
-//   IncrementItem = () => {
-//     this.setState((prevState) => {
-//       if (prevState.quantity < 20) {
-//         return {
-//           quantity: prevState.quantity + 1,
-//         };
-//       } else {
-//         return null;
-//       }
-//     });
-//   };
-
-//   DecreaseItem = () => {
-//     this.setState((prevState) => {
-//       if (prevState.quantity > 0) {
-//         return {
-//           quantity: prevState.quantity - 1,
-//         };
-//       } else {
-//         return null;
-//       }
-//     });
-//   };
-//   ToggleClick = () => {
-//     this.setState({
-//       show: !this.state.show,
-//     });
-//   };
-//   handleChange = (event) => {
-//     this.setState({ quantity: event.target.value });
-//   };
-
-//   render() {
-//     return (
-//       <div className="App">
-//         <button onClick={this.IncrementItem}>+</button>
-//         <input className="inputne" value={this.state.quantity} onChange={this.handleChange}/>
-//         <button onClick = {this.DecreaseItem}>-</button>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
-//Мой вариант конец
 
 //Вариант курса
 class App extends React.Component {
-  constructor() {
-    super();
+  state = {
+    posts: [],
+    loading: true,
+    comments: [],
+  };
 
-    this.state = {
-      count: 0,
-    };
+  componentDidMount() {
+    console.log("componentDidMount");
+    fetch("http://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => this.setState({ posts: data, loading: false }));
+
+    this.timerId = setInterval(() => {
+      fetch("http://jsonplaceholder.typicode.com/comments")
+        .then((response) => response.json())
+        .then((data) => this.setState({ comments: data }));
+    }, 3000);
   }
 
-  increment = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+  }
 
-  decrement = () => {
-    this.setState({ count: this.state.count - 1 });
-  };
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
 
   render() {
+    console.log("render", this.state.count);
     return (
-      <div className="App" style={{ margin: "auto", width: "300px" }}>
-        <button onClick={this.decrement}>-</button>
-        {/* Отображается текущее состояние ↓↓↓↓↓↓↓↓ */}
-        <span style={{ margin: "0 0.75rem", display: "inline-block" }}>
-          {" "}
-          {this.state.count}
-        </span>
-        <button onClick={this.increment}>+</button>
+      <div className="App">
+        {this.state.loading ? (
+          <h3>Lodaing...</h3>
+        ) : (
+          <h3>{this.state.posts.length}was loaded</h3>
+        )}
       </div>
     );
   }
